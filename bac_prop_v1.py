61c9ca494cebf389f723b7efc1ccccadb5976e05
MatanCohenG <Matangilad18@gmail.com>
2025-04-25 09:25:07 +0300

    Update bac_prop_v1.py

--- bac_prop_v1.py
+++ bac_prop_v1.py
@@ -6,7 +6,7 @@ import os
 """
 Introduction to Neural Network Shape Recognition Project
 
-
+The code was authored by the student Matan Cohen.
 
 Project Overview:
 This project focuses on the problem of geometric shape recognition using neural networks. Specifically, we are tasked with identifying three shapes: triangles, rectangles, and trapezoids. The goal is to train neural networks to accurately classify these shapes under varying conditions of size and position within a 10x10 matrix.
